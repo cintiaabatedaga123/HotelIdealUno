ef3889d5b1782a4d59924f53b2299be0d36bc32b
Veronica Albornoz <yazminn.vale@gmail.com>
2018-10-29 10:11:45 -0300

    reserva data

--- src/hotelidealuno/ReservaData.java
+++ src/hotelidealuno/ReservaData.java
@@ -62,7 +62,123 @@ public class ReservaData {
         }
     }
     
+     public List<Reserva> obtenerReservas(){
+        List<Reserva> reservas = new ArrayList<Reserva>();
+            
 
+        try {
+            String sql = "SELECT * FROM reserva;";
+            PreparedStatement statement = connection.prepareStatement(sql);
+            ResultSet resultSet = statement.executeQuery();
+            Reserva reserva;
+            while(resultSet.next()){
+                reserva = new Reserva();
+                reserva.setId(resultSet.getInt("id_reserva")); 
+                reserva.setCantidadPersonas(resultSet.getInt("cantidadPersonas"));
+                reserva.setFechaEntrada(resultSet.getDate("fechaEntrada").toLocalDate());
+                reserva.setFechaSalida(resultSet.getDate("fechaSalida").toLocalDate());
+                reserva.setImporteTotal(resultSet.getDouble("importeTotal"));
+                reserva.setEstadoReserva(resultSet.getBoolean("estadoReserva"));
+                reserva.setId(resultSet.getInt("id_habitacion"));
+                reserva.setId(resultSet.getInt("id_huesped"));
+                
     
-   
+                reservas.add(reserva);
+            }      
+            statement.close();
+        } catch (SQLException ex) {
+            System.out.println("Error al obtener las reserva: " + ex.getMessage());
+        }
+        
+        
+        return reservas;
+    }
+    
+    
+     public void borrarReserva(int id_huesped){
+    try {
+            
+            String sql = "DELETE FROM reserva WHERE id_huesped =?;";
+
+            PreparedStatement statement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            statement.setInt(1, id_huesped);
+           
+            
+            statement.executeUpdate();
+            
+            
+            statement.close();
+    
+        } catch (SQLException ex) {
+            System.out.println("Error al borrar una reserva: " + ex.getMessage());
+        }
+        
+    
+    }
+     
+     
+      public void actualizarReserva(Reserva reserva){
+    
+        try {
+            
+            String sql = "UPDATE reserva SET cantidadPersonas = ?, fechaEntrada = ?, fechaSalida = ? , importeTotal = ? , estadoReserva = ? , id_habitacion = ? , id_huesped = ? WHERE id_reserva = ?;";
+
+            PreparedStatement statement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            statement.setInt(1, reserva.getCantidadPersonas());
+            statement.setDate(2, Date.valueOf(reserva.getFechaEntrada()));
+            statement.setDate(3, Date.valueOf(reserva.getFechaSalida()));
+            statement.setDouble(4, reserva.getImporteTotal());
+            statement.setBoolean(5, reserva.getEstadoReserva());
+            statement.setInt(6, reserva.getId_habitacion());
+            statement.setInt(7, reserva.getId_huesped());
+            statement.setInt(8, reserva.getId());
+            statement.executeUpdate();
+    
+          
+            statement.close();
+    
+        } catch (SQLException ex) {
+            System.out.println("Error al actualizar una reserva : " + ex.getMessage());
+        }
+    
+    }
+    
+    public Reserva buscarReserva(int id_huesped){
+    Reserva reserva=null;
+    try {
+            
+            String sql = "SELECT * FROM reserva WHERE id_huesped =?;";
+
+            PreparedStatement statement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            statement.setInt(1, id_huesped);
+           
+            
+            ResultSet resultSet=statement.executeQuery();
+            
+            while(resultSet.next()){
+                reserva = new Reserva();
+                reserva.setId(resultSet.getInt("id_reserva"));
+                reserva.setCantidadPersonas(resultSet.getInt("cantidadPersonas"));
+                reserva.setFechaEntrada(resultSet.getDate("fechaEntrada").toLocalDate());
+                reserva.setFechaSalida(resultSet.getDate("fechaSalida").toLocalDate());
+                reserva.setImporteTotal(resultSet.getDouble("cantPersonasMax"));
+                reserva.setEstadoReserva(resultSet.getBoolean("cantCamas"));
+                reserva.setId(resultSet.getInt("id_habitacion"));
+                reserva.setId(resultSet.getInt("id_huesped"));
+                
+            }      
+            statement.close();
+            
+            
+            
+            
+    
+        } catch (SQLException ex) {
+            System.out.println("Error al buscar una reserva : " + ex.getMessage());
+        }
+        
+        return reserva;
+    }
+
+     
 }
